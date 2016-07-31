cf6902cb7984d1081d5a05253384c5075f0c0183
skriklgb <1071282757@qq.com>
2016-07-31 11:30:13 +0800

    03.ThinkPHP--模块化和URL模式-URL 模式，去掉index.php

--- Cms/Home/Controller/UserController.class.php
+++ Cms/Home/Controller/UserController.class.php
@@ -9,7 +9,16 @@ use Think\Controller;
 
 class UserController extends Controller{
     public function index(){
-        echo "user";
+        echo "index";
+    }
+    public function test($user,$pass){
+        echo "user=".$user."pass=".$pass;
+        //PATHINFO模式
+        //http://localhost/thinkphp/index.php/Home/User/test/user/less/pass/123     user=lesspass=123
+        //Home 表示模块，User 表示控制器，test 表示方法，user/Lee 表示第一个键值对，pass/123 表示第二个键值对。
+        //普通模式：http://localhost/thinkphp/index.php?m=Home&c=User&a=test&user=less&pass=123
+        //或者           http://localhost/thinkphp/?m=Home&c=User&a=test&user=less&pass=123
 
+        //去除了index.php    http://localhost/thinkphp/Home
     }
 }
